98ced08e68b79d1357b1dceed2bc891301d2010d
rohitprajapati0307 <r.k.prajapati0307@gmail.com>
2024-08-30 13:15:58 +0530

    home page css
    Home page css updated profile image and Animated Gradient Border

--- templates/home.py
+++ templates/home.py
@@ -12,8 +12,6 @@ def show_home():
         st.markdown('<h1 class="designation-txt">( Web Designer & Developer )</h1>', unsafe_allow_html=True)
         st.markdown('<p class="custom-description">a passionate and seasoned web developer who holds a BSc in Computer Science from Mumbai University. With a focus on creating clean, responsive, and user-friendly websites, I bring a wealth of experience and expertise in WordPress, HTML, CSS, PHP, and JavaScript.</p>', unsafe_allow_html=True)
         st.markdown('<p class="custom-description">Having had the privilege of contributing to reputable organizations, I am now excited to extend my services as a freelance web developer. My career has been shaped by a commitment to delivering high-quality, customized solutions.</p>', unsafe_allow_html=True)
-        # st.text("Having had the privilege of contributing to reputable organizations, I am now excited to extend my services as a freelance web developer. My career has been shaped by a commitment to delivering high-quality, customized solutions.")
-        # st.button("Click Me")
 
         # Custom button using HTML
         st.markdown('<a href="#about" class="custom-button link-item outer-shadow hover-in-shadow">More about Me</a>', unsafe_allow_html=True)
@@ -22,10 +20,11 @@ def show_home():
     with col2:
         st.markdown(
             """
-            <div class="image-container inner-shadow">
-                <img src="data/home_img/profile-pic.png" class="custom-image outer-shadow" alt="Placeholder Image">
-              
+            <div class="image-container outer-shadow">
+                <img src="https://raw.githubusercontent.com/rk-prajapati-37/blank-app/main/data/home_img/profile-pic.png" class="custom-image inner-shadow " alt="Profile Image">
             </div>
             """,
             unsafe_allow_html=True
         )
+
+
